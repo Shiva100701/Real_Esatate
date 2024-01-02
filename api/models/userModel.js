e2a3d2b54b3059c4f4ccd9be060680e9f32b34d0
Shivam Mishra <shivamproven110@gmail.com>
2024-01-02 22:11:00 +0530

    practiced backend route for better understanding.

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,22 +1,24 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password:{
-        type: String,
-        required: true,
-    },
-}, {timestamps: true});
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email:{
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    }, {timestamps: true} //timestamp automatically tracks the creation and updation of the user and it help us while sorting the user information.
+);
 
-const User = mongoose.model('User', userSchema)
+const user = mongoose.model('User', userSchema)
 
-export default User;
+export default user;
