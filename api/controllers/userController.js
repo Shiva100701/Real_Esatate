59ad348c868f88ddaa6b188b9b6a6dabf455d221
Shivam Mishra <shivamproven110@gmail.com>
2024-01-14 19:02:21 +0530

    compleate api route and UI of showlisting button

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listingModel.js"
 import user from "../models/userModel.js"
 import { errorHandler } from "../utils/error.js"
 import bcryptjs from 'bcryptjs'
@@ -34,7 +35,7 @@ export const updateUser = async (req, res, next) => {
 };
 
 export const deleteUser = async (req, res, next) =>{
- if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
+ if(req.id !== req.params.id) return next(errorHandler(401, 'You can only delete your own account!'))
  try {
     await user.findByIdAndDelete(req.params.id)
     res.clearCookie('access_token')
@@ -42,4 +43,17 @@ export const deleteUser = async (req, res, next) =>{
  } catch (error) {
     next(error)
  }
+}
+
+export const getUserListings = async (req, res, next) => {
+if(req.user.id === req.params.id){
+  try {
+    const listings = await Listing.find({userRef: req.params.id})
+    res.status(200).json(listings)
+  } catch (error) {
+    next(error)
+  }
+} else{
+  return next(errorHandler(401, 'You can only view your own listings!'))
+}
 }
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -16,6 +16,8 @@ function Profile() {
   const [formData, setFormData]= useState({})
   const dispatch = useDispatch()
   const [updatedSuccess, setUpdatedSuccess] = useState(false)
+ const [showListingsError, setShowListingsError] = useState(false)
+  const [userListings, setUserListings] = useState([])
 
   useEffect(()=>{
     if(file) {
@@ -107,53 +109,144 @@ function Profile() {
     } catch (error) {
       dispatch(signOutUserFailure(error.message))
     }
-  }
+  };
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
 
   return (
-    <div className='p-3 max-w-lg mx-auto'>
+    <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7"> Profile </h1>
-      <form className="flex flex-col gap-4"
-      onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
-        onChange={(e)=> setFile(e.target.files[0])}
-        type="file" 
-        ref={filereF} 
-        hidden 
-        accept='image/*'
+          onChange={(e) => setFile(e.target.files[0])}
+          type="file"
+          ref={filereF}
+          hidden
+          accept="image/*"
         />
         <img
-        onClick={()=> filereF.current.click()}
+          onClick={() => filereF.current.click()}
           src={formData.avatar || currentUser.avatar}
           alt="profile"
           className="self-center h-24 w-24 rounded-full object-cover mt-2 cursor-pointer "
         />
-        <p className='text-sm self-center'>
-        {
-        fileUploadError ? (<span className='text-red-700'>Error Image Upload. Please Try again....</span>) : filePer> 0 && filePer<100 ? (<span className='text-orange-700'>{`Uploading ${filePer}%`}</span>)
-        : filePer===100 ? (<span className='text-green-700'> Image Successfully Uploaded!</span>) :
-      ""}
+        <p className="text-sm self-center">
+          {fileUploadError ? (
+            <span className="text-red-700">
+              Error Image Upload. Please Try again....
+            </span>
+          ) : filePer > 0 && filePer < 100 ? (
+            <span className="text-orange-700">{`Uploading ${filePer}%`}</span>
+          ) : filePer === 100 ? (
+            <span className="text-green-700">
+              {" "}
+              Image Successfully Uploaded!
+            </span>
+          ) : (
+            ""
+          )}
         </p>
-        <input type="text" placeholder='username' id='username' className='border rounded-lg p-3' defaultValue={currentUser.username} onChange={handleChange}/>
-        <input type="email" placeholder='email' id='email' className='border rounded-lg p-3' defaultValue={currentUser.email} onChange={handleChange}/>
-        <input type="password" placeholder='password' id='password' className='border rounded-lg p-3'onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{
-          loading ? 'Loading...' : 'Update'
-        }
+        <input
+          type="text"
+          placeholder="username"
+          id="username"
+          className="border rounded-lg p-3"
+          defaultValue={currentUser.username}
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border rounded-lg p-3"
+          defaultValue={currentUser.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Update"}
         </button>
-        <Link className='text-white uppercase bg-green-700 p-3 rounded-lg text-center hover:opacity-95' to='/create-listing'>
+        <Link
+          className="text-white uppercase bg-green-700 p-3 rounded-lg text-center hover:opacity-95"
+          to="/create-listing"
+        >
           Create Listing
         </Link>
       </form>
       <div className="flex justify-between mt-4">
-        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
-        <span onClick={handleSignOut}className='text-red-700 cursor-pointer'>Sign out</span>
+        <span
+          onClick={handleDeleteUser}
+          className="text-red-700 cursor-pointer"
+        >
+          Delete Account
+        </span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">
+          Sign out
+        </span>
       </div>
-      <p className='text-red-700 mt-5'>
-        {error ? error : ''}
+      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5">
+        {updatedSuccess ? "User updated successfully" : ""}
       </p>
-      <p className='text-green-700 mt-5'>
-        {updatedSuccess ? "User updated successfully" : ''}
+
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show Listings
+      </button>
+      <p className="text-red-500 mt-5">
+        {showListingsError ? "Error showing Listing" : ""}
       </p>
+
+      {userListings &&
+        userListings.length > 0 &&
+        <div className="flex flex-col gap-4">
+<h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="border rounded-lg flex items-center justify-between p-3 gap-4 "
+            >
+              <Link to={`/listings/${listing._id}`}>
+                <img
+                  src={listing.imageUrls}
+                  alt=""
+                  className="w-16 h-16 object-contain"
+                />
+              </Link>
+              <Link
+                className="flex-1 text-slate-700 font-semibold hover:underline truncate "
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className='flex flex-col items-center'>
+                <button className="text-red-700 uppercase">Delete</button>
+                <button className="text-green-700 uppercase">edit</button>
+              </div>
+            </div>
+          ))}
+        </div>}
     </div>
   );
 }
